bdc7a7b00f2dbd22c553f92a5d3f40745931dd1a
Sergey <sergeygarnov1991@gmail.com>
2016-03-20 14:05:25 +0300

    universal app added but not implemented

---
+++ Bubbleshot.Server/BubbleShot.UniversalApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Prism.Unity.Windows;
+
+namespace BubbleShot.UniversalApp
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : PrismUnityApplication
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+			this.InitializeComponent();
+        }
+
+	    protected override Task OnLaunchApplicationAsync(Windows.ApplicationModel.Activation.LaunchActivatedEventArgs args)
+	    {
+			NavigationService.Navigate("Main", Container);
+			return Task.FromResult<object>(null);
+		}
+    }
+}
---
+++ Bubbleshot.Server/BubbleShot.UniversalApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Prism.Windows.Mvvm;
+
+namespace BubbleShot.UniversalApp.ViewModels
+{
+	public class MainPageViewModel : ViewModelBase
+	{
+
+	}
+}
