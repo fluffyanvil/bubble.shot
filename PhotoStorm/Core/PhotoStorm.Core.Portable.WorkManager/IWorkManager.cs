0b94f5c35a2251d961e8b0a3f9f608c36d78bba7
sergey.garnov <sergeygarnov1991@gmail.com>
2016-05-24 15:57:08 +0300

    add works and work manager

---
+++ PhotoStorm/Core/PhotoStorm.Core.Portable.WorkManager/IWorkManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using PhotoStorm.Core.Portable.Works.Works;
+
+namespace PhotoStorm.Core.Portable.WorkManager
+{
+    public interface IWorkManager
+    {
+        List<IWork> Works { get; set; }
+        void StartWork(IWork work);
+        void StopWork(IWork work);
+        void DeleteWork(IWork work);
+        void AddWork(IWork work);
+    }
+}
---
+++ PhotoStorm/Core/PhotoStorm.Core.Portable.Works/Works/IWork.cs
@@ -0,0 +1,15 @@
+﻿using PhotoStorm.Core.Portable.Adapters.Manager;
+using PhotoStorm.Core.Portable.Adapters.Rules;
+using PhotoStorm.Core.Portable.Works.Enums;
+
+namespace PhotoStorm.Core.Portable.Works.Works
+{
+    public interface IWork
+    {
+        IAdapterManager AdapterManager { get; set; }
+        IAdapterRule AdapterRule { get; set; }
+        WorkState State { get; set; }
+        void Start();
+        void Stop();
+    }
+}
